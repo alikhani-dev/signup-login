1670119673a520d9204fdbe578f3d50cada6a173
alikhani <alikhani.dev@gmail.com>
2022-09-22 23:40:52 +0430

    [Add] : prettierignore config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	tabWidth: 2,
+	semi: false,
+	singleQuote: true,
+	useTabs: true,
+	trailingComma: 'all',
+	arrowParens: 'avoid',
+	printWidth: 120,
+	jsxSingleQuote: true,
+	bracketSpacing: true
+}
